fcea589cf2f8fd339ed0e3f99309a2b09479e744
neda <nedaraad@outlook.com>
2020-12-26 11:00:16 +0330

    Get the name of the input & output file from user

--- table_extract/table.py
+++ table_extract/table.py
@@ -1,6 +1,8 @@
 import tabula
 
 
-file = "input.pdf"
+finp = input("Enter your pdf file name: ")
+fout = input("Enter your output file name: ")
+
 # extract table from pdf file to csv file
-tabula.convert_into(file, "output.csv", pages="all")
+tabula.convert_into(finp, fout, pages="1")
